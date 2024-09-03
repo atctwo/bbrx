33e329a464d067e83f9752b25d92c5886ca39094
atctwo <atctwopointfive@gmail.com>
2024-09-03 13:58:22 +0100

    added weapon control binds

--- bbrx/config.h
+++ bbrx/config.h
@@ -18,18 +18,22 @@
 //  Bot Control Settings
 
 // Default Bindings
-#define BB_DEFAULT_BINDINGS {                                                                           \
-                                                                                                        \
-    // movement (servo ch1 + ch2)                                                                       \
-    {.action = BB_ACTION_SERVO, .event = BB_EVENT_ANALOG_LY, .min = -512, .max =  511, .pin = 12},      \
-    {.action = BB_ACTION_SERVO, .event = BB_EVENT_ANALOG_LX, .min =  511, .max = -512, .pin = 13},      \
-                                                                                                        \
-    // brake                                                                                            \
-    {.action = BB_ACTION_BREAK, .event = BB_EVENT_BTN_A, .min = 0, .max = 1},                           \
-                                                                                                        \
-    // speed controls                                                                                   \
-    {.action = BB_ACTION_SPEED_UP, .event = BB_EVENT_BTN_B, .min = 0, .max = 1},                        \
-    {.action = BB_ACTION_SPEED_DOWN, .event = BB_EVENT_BTN_X, .min = 0, .max = 1},                      \
+#define BB_DEFAULT_BINDINGS {                                                                                   \
+                                                                                                                \
+    /* movement (servo ch1 + ch2) */                                                                            \
+    {.action = BB_ACTION_SERVO, .event = BB_EVENT_ANALOG_LY, .min = -512, .max =  511, .pin = 12},              \
+    {.action = BB_ACTION_SERVO, .event = BB_EVENT_ANALOG_LX, .min =  511, .max = -512, .pin = 13},              \
+                                                                                                                \
+    /* weapon control */                                                                                        \
+    {.action = BB_ACTION_SERVO, .event = BB_EVENT_ANALOG_BRAKE,    .min =  -1024, .max =  1023, .pin = 14},     \
+    {.action = BB_ACTION_SERVO, .event = BB_EVENT_ANALOG_THROTTLE, .min =   1023, .max = -1024, .pin = 14},     \
+                                                                                                                \
+    /* brake */                                                                                                 \
+    {.action = BB_ACTION_BREAK, .event = BB_EVENT_BTN_A, .min = 0, .max = 1},                                   \
+                                                                                                                \
+    /* speed control */                                                                                         \
+    {.action = BB_ACTION_SPEED_UP, .event = BB_EVENT_BTN_B, .min = 0, .max = 1},                                \
+    {.action = BB_ACTION_SPEED_DOWN, .event = BB_EVENT_BTN_X, .min = 0, .max = 1},                              \
 }
 
 // Deadzones and Beefzones
